600b445813a5bdddaf4c60ca10ccaf7379386f7f
jaeyeonjung <jaeyeonjung3@gmail.com>
2024-12-13 13:23:23 +0900

    [fix] run prettier

--- src/styles/ThemeConfig.ts
+++ src/styles/ThemeConfig.ts
@@ -1,9 +1,9 @@
-import { css } from "styled-components";
+import { css } from 'styled-components';
 const typoCreator = (
   fontSize: string,
   fontWeight: number,
   letterSpacing: string,
-  lineHeight: string
+  lineHeight: string,
 ) => {
   return css`
     font-size: ${fontSize};
@@ -14,18 +14,17 @@ const typoCreator = (
 };
 export const ThemeConfig = {
   colors: {
-    white: "#FFFFFF",
-    black: "#000000",
+    white: '#FFFFFF',
+    black: '#000000',
   },
-  shadows: {
-  },
+  shadows: {},
   typo: {
-    desktop_title: typoCreator("8rem", 900, "normal", "normal"),
-    desktop_subtitle: typoCreator("1.5rem", 900, "normal", "normal"),
-    desktop_body_B: typoCreator("1.25rem", 700, "normal", "normal"),
-    desktop_body_R: typoCreator("1.25rem", 400, "normal", "normal"),
-    desktop_impt: typoCreator("4rem", 900, "normal", "normal"),
-    desktop_mode: typoCreator("2rem", 900, "normal", "normal"),
-  }
-}
-export type TColor = keyof (typeof ThemeConfig)["colors"];
+    desktop_title: typoCreator('8rem', 900, 'normal', 'normal'),
+    desktop_subtitle: typoCreator('1.5rem', 900, 'normal', 'normal'),
+    desktop_body_B: typoCreator('1.25rem', 700, 'normal', 'normal'),
+    desktop_body_R: typoCreator('1.25rem', 400, 'normal', 'normal'),
+    desktop_impt: typoCreator('4rem', 900, 'normal', 'normal'),
+    desktop_mode: typoCreator('2rem', 900, 'normal', 'normal'),
+  },
+};
+export type TColor = keyof (typeof ThemeConfig)['colors'];
