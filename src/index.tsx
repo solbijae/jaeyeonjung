1d6af001ee18eba82bf88348143faa05bb4706f6
jaeyeonjung <jaeyeonjung3@gmail.com>
2024-12-13 13:33:18 +0900

    [fix] setup index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import mediaQueries from './styles/MediaQuery';
+import { ThemeConfig } from './styles/ThemeConfig';
+import { GlobalStyle } from './styles/GlobalStyle';
 import App from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <ThemeProvider theme={{ ...ThemeConfig, mediaQueries }}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
+  </BrowserRouter>,
 );
