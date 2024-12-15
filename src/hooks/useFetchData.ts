6f47ac35240720871f42422022641535327ccab0
Jae <jaeyeonjung3@gmail.com>
2024-12-15 23:11:20 +0900

    [feat] add useFetchData

---
+++ src/hooks/useFetchData.ts
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+
+function useFetchData<T>(url: string){
+  const [data, setData] = useState<T | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((err) => setError(err));
+  }, [url]);
+
+  return { data, error }
+}
+
+export default useFetchData;
