d25b4286a5971ee73d7237f4340e96e3caa779b1
jaeyeonjung <jaeyeonjung3@gmail.com>
2024-12-16 17:50:24 +0900

    [feat] add Contact

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,11 +1,32 @@
 import * as S from './styles';
+import TransparentText from '../common/Text/Transparent';
 
 const Contact = () => {
   return (
     <S.Container>
-
+      <TransparentText>
+        <S.ContactInfo href="mailto:jaeyeonjung3@gmail.com">EMAIL</S.ContactInfo>
+      </TransparentText>
+      <TransparentText>
+        <S.ContactInfo
+          href="https://www.linkedin.com/in/jungjaeyeon/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LINKEDIN
+        </S.ContactInfo>
+      </TransparentText>
+      <TransparentText>
+        <S.ContactInfo
+          href="https://github.com/solbijae"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GITHUB
+        </S.ContactInfo>
+      </TransparentText>
     </S.Container>
   );
-}
+};
 
 export default Contact;
--- src/components/Contact/styles.ts
+++ src/components/Contact/styles.ts
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-
+  display: flex;
+  gap: ${({ theme }) => theme.margin.desktop_text_gap};
+  margin-left: ${({ theme }) => theme.margin.desktop_section_indent};
+  padding: ${({ theme }) => theme.padding.desktop_section} 0;
 `;
+
+export const ContactInfo = styled.a``;
