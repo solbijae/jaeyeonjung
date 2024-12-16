c4909012afc72fcc74d285625d4e6de0ec4ced57
jaeyeonjung <jaeyeonjung3@gmail.com>
2024-12-16 17:50:08 +0900

    [feat add EduCert

--- src/components/EduCert/index.tsx
+++ src/components/EduCert/index.tsx
@@ -1,11 +1,27 @@
 import * as S from './styles';
+import useFetchData from '../../hooks/useFetchData';
+import { EducertType } from '../../types/educert';
+import TransparentText from '../common/Text/Transparent';
 
 const EduCert = () => {
+  const { data } = useFetchData<EducertType>('/data/educert.json');
+
   return (
     <S.Container>
-
+      <TransparentText>
+        {data?.edu_cert &&
+          data.edu_cert.map((datum) => (
+            <S.EduCertWrap>
+              <S.Title>{datum.title}</S.Title>
+              <S.Detail>
+                <S.Institution>{datum.institution}&nbsp;</S.Institution>
+                <S.Date>({datum.year})</S.Date>
+              </S.Detail>
+            </S.EduCertWrap>
+          ))}
+      </TransparentText>
     </S.Container>
   );
-}
+};
 
 export default EduCert;
--- src/components/EduCert/styles.ts
+++ src/components/EduCert/styles.ts
@@ -1,5 +1,23 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-
+  display: flex;
+  flex-direction: column;
+  gap: ${({ theme }) => theme.margin.desktop_text_gap};
+  margin-left: ${({ theme }) => theme.margin.desktop_section_indent};
+  padding: ${({ theme }) => theme.padding.desktop_section} 0;
 `;
+
+export const EduCertWrap = styled.div`
+  padding-bottom: ${({ theme }) => theme.padding.desktop_text_inner};
+`;
+
+export const Title = styled.p`
+  ${({ theme }) => theme.typo.desktop_body_16_B};
+`;
+
+export const Detail = styled.p``;
+
+export const Institution = styled.span``;
+
+export const Date = styled.span``;
