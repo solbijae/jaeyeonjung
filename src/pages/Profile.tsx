3f24b69dbe763b3ab8608b9492e543bcc0a5cfb9
jaeyeonjung <jaeyeonjung3@gmail.com>
2024-12-16 17:53:50 +0900

    [feat] add Experience, EduCert, Contact, Mode, Footer to Profile page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,18 +1,34 @@
-import Title from '../components/common/TItle';
+import Title from '../components/common/Title';
 import Description from '../components/Description';
 import SectionTitle from '../components/common/SectionTitle';
 import Skills from '../components/Skills';
+import Experience from '../components/Experience';
+import EduCert from '../components/EduCert';
+import Contact from '../components/Contact';
+import Mode from '../components/common/Mode';
+import Footer from '../components/common/Footer';
 
 const Profile = () => {
-  return(
+  return (
     <>
       <Title />
       <Description />
-      <SectionTitle name={"SKILLS"}>
+      <SectionTitle name={'SKILLS'}>
         <Skills />
       </SectionTitle>
+      <SectionTitle name={'EXPERIENCE'}>
+        <Experience />
+      </SectionTitle>
+      <SectionTitle name={'EDUCATION / CERTIFICATION'}>
+        <EduCert />
+      </SectionTitle>
+      <SectionTitle name={'CONTACT'}>
+        <Contact />
+      </SectionTitle>
+      <Mode />
+      <Footer />
     </>
   );
-}
+};
 
 export default Profile;
