229f800866665b5309bfd35953366810bc367c6a
jaeyeonjung <jaeyeonjung3@gmail.com>
2024-12-16 17:49:52 +0900

    [feat] add Experience

--- src/components/Experience/index.tsx
+++ src/components/Experience/index.tsx
@@ -1,11 +1,57 @@
 import * as S from './styles';
+import { useEffect } from 'react';
+import useFetchData from '../../hooks/useFetchData';
+import { ExperienceType } from '../../types/experience';
+import TransparentText from '../common/Text/Transparent';
 
 const Experience = () => {
+  const { data } = useFetchData<ExperienceType>('/data/experience.json');
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
   return (
     <S.Container>
-
+      {data?.experience &&
+        data.experience.map((item, index) => (
+          <S.ItemWrap>
+            <S.Company>{item.company}</S.Company>
+            <TransparentText key={index}>
+              <S.ItemTitleWrap>
+                <S.Position>{item.position} &nbsp;</S.Position>
+                <S.Dates>({item.dates})</S.Dates>
+              </S.ItemTitleWrap>
+              {item.roles && (
+                <S.Roles>
+                  {item.roles.map((role, roleIndex) => (
+                    <S.Role key={roleIndex}>
+                      <S.RoleTitle>{role.title}</S.RoleTitle>
+                      <S.Responsibilities>
+                        {role.responsibilities.map((resp, respIndex) => (
+                          <S.Responsibility key={respIndex}>
+                            - {resp}
+                          </S.Responsibility>
+                        ))}
+                      </S.Responsibilities>
+                    </S.Role>
+                  ))}
+                </S.Roles>
+              )}
+              {item.responsibilities && (
+                <S.Responsibilities>
+                  {item.responsibilities.map((resp, respIndex) => (
+                    <S.Responsibility key={respIndex}>
+                      - {resp}
+                    </S.Responsibility>
+                  ))}
+                </S.Responsibilities>
+              )}
+            </TransparentText>
+          </S.ItemWrap>
+        ))}
     </S.Container>
   );
-}
+};
 
 export default Experience;
--- src/components/Experience/styles.ts
+++ src/components/Experience/styles.ts
@@ -1,5 +1,47 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: ${({ theme }) => theme.margin.desktop_text_gap};
+  margin-left: ${({ theme }) => theme.margin.desktop_section_indent};
+  padding: ${({ theme }) => theme.padding.desktop_section} 0;
+  ${({ theme }) => theme.typo.desktop_body_16_R};
+`;
+
+export const ItemWrap = styled.div`
+  &:not(:first-child) {
+    padding-top: ${({ theme }) => theme.padding.desktop_section_title};
+  }
+`
+
+export const Company = styled.p`
+  padding-bottom: ${({ theme }) => theme.padding.desktop_section_title};
+  ${({ theme }) => theme.typo.desktop_body_16_B};
+`;
+
+export const ItemTitleWrap = styled.div`
+  ${({ theme }) => theme.typo.desktop_body_16_B};
+  padding-bottom: ${({ theme }) => theme.padding.desktop_text_inner};
+`;
+
+export const Position = styled.span`
+
+`;
+
+export const Dates = styled.span`
+
+`;
+
+export const Roles = styled.div``;
+
+export const Role = styled.div``;
+
+export const RoleTitle = styled.p``;
+
+export const Responsibilities = styled.ul``;
+
+export const Responsibility = styled.li`
+  padding-left: ${({ theme }) => theme.padding.desktop_text_inner};
 
 `;
