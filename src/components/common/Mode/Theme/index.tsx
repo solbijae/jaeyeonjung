791592b28616999a273d13b679e6bf80765c5faa
jaeyeonjung <jaeyeonjung3@gmail.com>
2024-12-16 17:51:55 +0900

    [feat] add Theme

--- src/components/common/Mode/Theme/index.tsx
+++ src/components/common/Mode/Theme/index.tsx
@@ -1,11 +1,32 @@
 import * as S from './styles';
+import { useState } from 'react';
 
 const Theme = () => {
+  const [profileMode, setProfileMode] = useState(true);
+
+  const toggleHandler = () => {
+    setProfileMode((prev) => !prev);
+  };
+
   return (
     <S.Container>
-
+      <S.CheckBox
+        type="checkbox"
+        id="toggleBtn"
+        onChange={toggleHandler}
+        checked={profileMode}
+      />
+      <S.ButtonLabel htmlFor="toggleBtn">
+        <S.ToggleCircle profileMode={profileMode}>
+          <S.CircleText profileMode={profileMode}>
+            {profileMode 
+              ? Array(10).fill('PROFILE').join(' ')
+              : Array(10).fill('WORK').join(' ')}
+          </S.CircleText>
+        </S.ToggleCircle>
+      </S.ButtonLabel>
     </S.Container>
   );
-}
+};
 
 export default Theme;
--- src/components/common/Mode/Theme/styles.ts
+++ src/components/common/Mode/Theme/styles.ts
@@ -1,5 +1,61 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
+export const CheckBox = styled.input`
+  display: none;
+`;
+
+export const ButtonLabel = styled.label`
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  padding: 5px;
+  align-items: center;
+  width: 162px;
+  height: 48px;
+  border-radius: 50px;
+  background-color: ${({ theme }) => theme.colors.black};
+  cursor: pointer;
+`;
+
+export const ToggleCircle = styled.div<{ profileMode: boolean }>`
+  position: absolute;
+  top: 50%;
+  left: ${({ profileMode }) => (profileMode ? '10px' : '57px')};
+  transform: translateY(-50%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  white-space: nowrap;
+  width: 90px;
+  height: 38px;
+  background-color:  ${({ theme }) => theme.colors.white};;
+  border-radius: 50px;
+  transition: left 0.3s ease;
+`;
+
+const marquee = keyframes`
+  0% {
+    transform: translate3d(0, 0, 0);
+  }
+  50% {
+    transform: translate3d(-20%, 0, 0);
+  }
+  100% {
+    transform: translate3d(-40%, 0, 0);
+  }
+`;
+
+export const CircleText = styled.span<{ profileMode: boolean }>`
+  will-change: transform;
+  display: inline-block;
+  animation: ${marquee} 30s linear infinite;
+  white-space: nowrap;
+  color: ${({ theme }) => theme.colors.black};
 `;
