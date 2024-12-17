dbbfb0fc4242236bb0baee01d959d81cd1703470
jaeyeonjung <jaeyeonjung3@gmail.com>
2024-12-17 17:24:13 +0900

    [fix] change TransParent style to use ThemeConfig

--- src/components/common/Text/Transparent/styles.ts
+++ src/components/common/Text/Transparent/styles.ts
@@ -3,6 +3,6 @@ import styled from 'styled-components';
 export const Container = styled.div`
   padding: ${({ theme }) => theme.padding.desktop_text_inner};
   margin-right: ${({ theme }) => theme.margin.desktop_text_gap};
-  border: 1px solid black;
+  border: 1px solid ${({ theme }) => theme.colors.black};
   border-radius: 5px;
 `;
