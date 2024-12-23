886b908316eebd43351f2e9354fa5383d5cc32bd
jaeyeonjung <jaeyeonjung3@gmail.com>
2024-12-23 16:11:17 +0900

    [style] make mode position fixed

--- src/components/common/Mode/styles.ts
+++ src/components/common/Mode/styles.ts
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  position: fixed;
+  bottom: 70px;
+  left: 50%;
+  transform: translateX(-50%);
   display: flex;
   justify-content: space-between;
   align-items: center;
