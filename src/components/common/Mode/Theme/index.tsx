27e2d539ec3fc6f07d96bf7faefbb7192f722d97
jaeyeonjung <jaeyeonjung3@gmail.com>
2024-12-26 12:43:59 +0900

    [style] add theme semicircle style

--- src/components/common/Mode/Theme/index.tsx
+++ src/components/common/Mode/Theme/index.tsx
@@ -1,10 +1,17 @@
 import * as S from './styles';
+import { useState } from 'react';
 
 const Theme = () => {
+  const [rotation, setRotation] = useState(0);
+
+  const handleClick = () => {
+    setRotation(prev => prev + 180);
+  }
+
   return (
     <S.Container>
-      <S.Circle>
-        <S.Semicircle />
+      <S.Circle onClick={handleClick}>
+        <S.Semicircle $rotation={rotation} />
       </S.Circle>
     </S.Container>
   );
--- src/components/common/Mode/Theme/styles.ts
+++ src/components/common/Mode/Theme/styles.ts
@@ -1,6 +1,28 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
-export const Container = styled.div``;
+const initialRotation = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const slideIn = keyframes`
+  from {
+    transform: translate(-10px, -50%);
+    opacity: 0;
+  }
+  to {
+    transform: translate(0, -50%);
+    opacity: 1;
+  }
+`;
+
+export const Container = styled.div`
+  position: relative;
+`;
 
 export const Circle = styled.div`
   width: 34px;
@@ -8,11 +30,31 @@ export const Circle = styled.div`
   background-color: ${({ theme }) => theme.colors.white};
   border: 1px solid ${({ theme }) => theme.colors.black};
   border-radius: 50%;
+  cursor: pointer;
+  position: relative;
+
+  &::after {
+    content: "CHANGE THEME";
+    position: absolute;
+    top: 50%;
+    left: 40px;
+    opacity: 0;
+    white-space: nowrap;
+    animation: none;
+  }
+
+  &:hover::after {
+    animation: ${slideIn} 0.3s ease forwards;
+  }
 `;
 
-export const Semicircle = styled.div`
+export const Semicircle = styled.div<{ $rotation: number}>`
   width: 17px;
   height: 34px;
   background-color: ${({ theme }) => theme.colors.black};
   border-radius: 17px 0 0 17px;
+  transition: transform 0.5s ease;
+  transform-origin: right center;
+  animation: ${initialRotation} 0.8s ease;
+  transform: ${({ $rotation }) => `rotate(${$rotation}deg)`};
 `;
