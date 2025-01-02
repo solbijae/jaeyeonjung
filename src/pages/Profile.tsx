5bddac74df8d34bbe6c64a4037f12e3cfb0a39a8
jaeyeonjung <jaeyeonjung3@gmail.com>
2025-01-02 14:05:23 +0900

    [test] testing source path for netlify

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,13 +1,12 @@
-// import Title from 'components/common/Title';
-import Title from 'components/common/Title';
-import Description from 'components/Description';
-import SectionTitle from 'components/common/SectionTitle';
-import Skills from 'components/Skills';
-import Experience from 'components/Experience';
-import EduCert from 'components/EduCert';
-import Contact from 'components/Contact';
-import Mode from 'components/common/Mode';
-import Footer from 'components/common/Footer';
+import Title from '../components/common/Title';
+import Description from '../components/Description';
+import SectionTitle from '../components/common/SectionTitle';
+import Skills from '../components/Skills';
+import Experience from '../components/Experience';
+import EduCert from '../components/EduCert';
+import Contact from '../components/Contact';
+import Mode from '../components/common/Mode';
+import Footer from '../components/common/Footer';
 
 const Profile = () => {
   return (
